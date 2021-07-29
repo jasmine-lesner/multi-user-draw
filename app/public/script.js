bd09ba7dffcde3c5a3cda16422902d064d4f4f2e
Jasmine Lesner <jazzy.lesner@gmail.com>
2021-07-29 16:50:41 -0700

    polishing done

--- app/public/script.js
+++ app/public/script.js
@@ -4,14 +4,16 @@ var socket;
 
 let h;
 let palette;
+let brushSize;
 
 function setup() {
     createCanvas(700, 500);
     colorMode(HSB, 360, 100, 100);
     background(50, 5, 100);
     socket = io.connect('http://localhost:3000');
-    socket.on('mouse', newDrawing);
+    socket.on('mouse', newDrawing); // Recieving the partner's data and sending it to a funtion
     h = 0;
+    brushSize = 30;
     palette = [];
 
     for (let i = 0; i < width / paletteSize; i++) {
@@ -24,6 +26,7 @@ function setup() {
     //console.log(palette);
 }
 
+// Individual colors in the palette
 class pigment {
     constructor(h, x, y, id) {
         this.h = h;
@@ -40,7 +43,7 @@ class pigment {
 function draw() {
     noStroke();
     fill(h, 100, 100);
-    square(0, 0, paletteSize);
+    circle(paletteSize, paletteSize, brushSize);
 
     for (let hue of palette) {
         fill(hue.h, 100, 100);
@@ -49,9 +52,10 @@ function draw() {
 }
 
 function newDrawing(data) {
+    // Partner's Drawing
     noStroke();
     fill(data.h, 50, 100);
-    ellipse(data.x, data.y, 36, 36);
+    ellipse(data.x, data.y, data.brushSize, data.brushSize);
 }
 
 function mouseDragged() {
@@ -60,15 +64,18 @@ function mouseDragged() {
     var data = {
         x: mouseX,
         y: mouseY,
-        h: h
+        h: h, // Hue
+        brushSize: brushSize
     };
-    socket.emit('mouse', data);
+    socket.emit('mouse', data); // sending the brush's data thru socket 
 
+    // User's Drawing
     noStroke();
     fill(h, 100, 100);
-    ellipse(mouseX, mouseY, 36, 36);
+    ellipse(mouseX, mouseY, brushSize, brushSize);
 }
 
+// Double clicking on a color to change the brush hue
 function doubleClicked() {
     console.log("clicked");
     for (let hue of palette) {
@@ -80,17 +87,41 @@ function doubleClicked() {
     }
 }
 
-// function keyPressed() {
-//     if (keyCode === 82) {
-//         console.log("red");
-//         setHue(0);
-//     }
-//     if (keyCode === 66) {
-//         console.log("blue");
-//         h = 230;
-//     }
-//     if (keyCode === 71) {
-//         console.log("green");
-//         h = 120;
-//     }
-// }
+function keyPressed() {
+    // Key presses to change color
+
+    // if (keyCode === 82) {
+    //     console.log("red");
+    //     setHue(0);
+    // }
+    // if (keyCode === 66) {
+    //     console.log("blue");
+    //     h = 230;
+    // }
+    // if (keyCode === 71) {
+    //     console.log("green");
+    //     h = 120;
+    // }
+
+    // Changing the brush size wit "[" and "]"
+    if (keyCode === 221) {
+        if (brushSize >= 80) {
+            brushSize = 80;
+        } else {
+            fill(50, 5, 100);
+            circle(paletteSize, paletteSize, brushSize + 2);
+            brushSize += 5;
+            console.log("increase brush size to " + brushSize);
+        }
+    }
+    if (keyCode === 219) {
+        if (brushSize <= 5) {
+            brushSize = 5;
+        } else {
+            fill(50, 5, 100);
+            circle(paletteSize, paletteSize, brushSize + 2);
+            brushSize -= 5;
+            console.log("decrease brush size to " + brushSize);
+        }
+    }
+}
