8e2496768f28f8202ef0fd55afb2b16245565597
Jasmine Lesner <jazzy.lesner@gmail.com>
2021-07-29 14:05:10 -0700

    completed tutorial

--- app/server.js
+++ app/server.js
@@ -17,6 +17,8 @@ function newConnection(socket) {
     console.log('new connection: ' + socket.id);
     socket.on('mouse', mouseMsg)
     function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        // io.sockets.emit('mouse', data); <-- broadcasts the message to all clients (including the one emitting)
         console.log(data);
     }
 }
