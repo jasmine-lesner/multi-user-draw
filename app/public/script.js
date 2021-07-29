10f3235617b4267bd20eaaa2914fa5a5101566d2
Jasmine Lesner <jazzy.lesner@gmail.com>
2021-07-29 15:21:04 -0700

    converted to HSB

--- app/public/script.js
+++ app/public/script.js
@@ -1,62 +1,57 @@
 var socket;
-let r, g, b;
+let h;
 
 function setup() {
     createCanvas(600, 400);
     background(51);
+    colorMode(HSB, 360, 100, 100);
     socket = io.connect('http://localhost:3000');
     socket.on('mouse', newDrawing);
-    r = 0;
-    g = 255;
-    b = 0;
+    h = 0;
+}
+
+// class pigment {
+
+// }
+
+function draw() {
+    noStroke();
+    fill(h, 100, 100);
+    square(0, 0, 50);
 }
 
 function newDrawing(data) {
     noStroke();
-    fill(data.r, data.g, data.b);
+    fill(data.h, 50, 100);
     ellipse(data.x, data.y, 36, 36);
 }
 
 function mouseDragged() {
-    console.log('Sending: (' + mouseX + ', ' + mouseY + ') and Color: (' + r + ', ' + g + ', ' + b + ')');
+    console.log('Sending: (' + mouseX + ', ' + mouseY + ') and Color: (' + h + ')');
 
     var data = {
         x: mouseX,
         y: mouseY,
-        r: r,
-        g: g,
-        b: b
+        h: h
     };
     socket.emit('mouse', data);
 
     noStroke();
-    fill(r, g, b);
+    fill(h, 100, 100);
     ellipse(mouseX, mouseY, 36, 36);
-}
-
-function draw() {
-    noStroke();
-    fill(r, g, b);
-    square(0, 0, 50);
 }
 
 function keyPressed() {
     if (keyCode === 82) {
         console.log("red");
-        r = 255;
-        g = 0;
-        b = 0;
+        h = 0;
     }
     if (keyCode === 66) {
         console.log("blue");
-        r = 0;
-        g = 0;
-        b = 255;
+        h = 230;
     }
     if (keyCode === 71) {
         console.log("green");
-        r = 0;
-        g = 255;
-        b = 0;
+        h = 120;
     }
 }
