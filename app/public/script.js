b6b352f983c544c623d2b26a56fda164cb5ff364
Jasmine Lesner <jazzy.lesner@gmail.com>
2021-07-29 14:37:41 -0700

    sending rgb values

--- app/public/script.js
+++ app/public/script.js
@@ -1,29 +1,36 @@
 var socket;
+let r, g, b;
 
 function setup() {
     createCanvas(600, 400);
     background(51);
     socket = io.connect('http://localhost:3000');
     socket.on('mouse', newDrawing);
+    r = 0;
+    g = 255;
+    b = 0;
 }
 
 function newDrawing(data) {
     noStroke();
-    fill(255,0,100);
+    fill(data.r, data.g, data.b);
     ellipse(data.x, data.y, 36, 36);
 }
 
 function mouseDragged() {
-    console.log('Sending: (' + mouseX + ', ' + mouseY + ')')
+    console.log('Sending: (' + mouseX + ', ' + mouseY + ') and Color: (' + r + ', ' + g + ', ' + b + ')');
 
     var data = {
         x: mouseX,
-        y: mouseY
+        y: mouseY,
+        r: r,
+        g: g,
+        b: b
     };
     socket.emit('mouse', data);
 
     noStroke();
-    fill(255);
+    fill(r, g, b);
     ellipse(mouseX, mouseY, 36, 36);
 }
 
